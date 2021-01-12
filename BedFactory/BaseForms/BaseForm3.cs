8e99325829c6a4a5f14ec1872fdf592b9cd63030
장경진 <71303347+JGJ0403@users.noreply.github.com>
2021-01-12 18:19:43 +0900

    장경진업로드

---
+++ BedFactory/BaseForms/BaseForm3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BedFactory.BaseForms
+{
+    public partial class BaseForm3 : Form
+    {
+        public BaseForm3()
+        {
+            InitializeComponent();
+        }
+    }
+}
