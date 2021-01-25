6123a818e3a0406f97f99b681b9f21c7f303dd77
경진 장 <jkj04032000@gmail.com>
2021-01-25 15:53:45 +0900

    베이스폼수정

--- BedFactory/BaseForms/BaseForm4.Designer.cs
+++ BedFactory/BaseForms/BaseForm4.Designer.cs
@@ -30,18 +30,17 @@ namespace BedFactory.BaseForms
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelComboText1 = new BedFactory.LabelComboText();
+            this.labelComboText2 = new BedFactory.LabelComboText();
+            this.labelComboText3 = new BedFactory.LabelComboText();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,28 +50,14 @@ namespace BedFactory.BaseForms
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelComboText3);
+            this.panel1.Controls.Add(this.labelComboText2);
+            this.panel1.Controls.Add(this.labelComboText1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1358, 65);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.Location = new System.Drawing.Point(12, 130);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1358, 711);
-            this.panel2.TabIndex = 1;
             // 
             // button1
             // 
@@ -88,12 +73,23 @@ namespace BedFactory.BaseForms
             this.button1.Text = "조회";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Location = new System.Drawing.Point(12, 130);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1358, 711);
+            this.panel2.TabIndex = 1;
+            // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button4.Location = new System.Drawing.Point(1305, 98);
+            this.button4.Location = new System.Drawing.Point(1305, 99);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(65, 25);
             this.button4.TabIndex = 4;
@@ -104,7 +100,7 @@ namespace BedFactory.BaseForms
             this.button5.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button5.Location = new System.Drawing.Point(1199, 98);
+            this.button5.Location = new System.Drawing.Point(1057, 99);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 25);
             this.button5.TabIndex = 3;
@@ -114,7 +110,8 @@ namespace BedFactory.BaseForms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(12, 98);
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 25);
             this.label1.TabIndex = 5;
@@ -126,77 +123,81 @@ namespace BedFactory.BaseForms
             this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button2.Location = new System.Drawing.Point(1068, 98);
+            this.button2.Location = new System.Drawing.Point(926, 99);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 25);
             this.button2.TabIndex = 6;
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // label2
+            // labelComboText1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(27, 7);
-            this.label2.Margin = new System.Windows.Forms.Padding(7);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.labelComboText1.BackColor = System.Drawing.Color.Transparent;
+            this.labelComboText1.BoxKind = BedFactory.LabelComboText.Kind.combo;
+            this.labelComboText1.ComboData = null;
+            this.labelComboText1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.labelComboText1.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.labelComboText1.Header = null;
+            this.labelComboText1.Important = false;
+            this.labelComboText1.Location = new System.Drawing.Point(5, 3);
+            this.labelComboText1.MultiLine = false;
+            this.labelComboText1.MultiLineCnt = 0;
+            this.labelComboText1.Name = "labelComboText1";
+            this.labelComboText1.Size = new System.Drawing.Size(398, 31);
+            this.labelComboText1.TabIndex = 7;
             // 
-            // label3
+            // labelComboText2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(10, 10);
-            this.label3.Margin = new System.Windows.Forms.Padding(10, 10, 10, 3);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 23);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "*";
+            this.labelComboText2.BackColor = System.Drawing.Color.Transparent;
+            this.labelComboText2.BoxKind = BedFactory.LabelComboText.Kind.combo;
+            this.labelComboText2.ComboData = null;
+            this.labelComboText2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.labelComboText2.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.labelComboText2.Header = null;
+            this.labelComboText2.Important = false;
+            this.labelComboText2.Location = new System.Drawing.Point(437, 3);
+            this.labelComboText2.MultiLine = false;
+            this.labelComboText2.MultiLineCnt = 0;
+            this.labelComboText2.Name = "labelComboText2";
+            this.labelComboText2.Size = new System.Drawing.Size(398, 31);
+            this.labelComboText2.TabIndex = 8;
             // 
-            // label6
+            // labelComboText3
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label6.Location = new System.Drawing.Point(1034, 10);
-            this.label6.Margin = new System.Windows.Forms.Padding(10, 10, 10, 3);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(17, 23);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "*";
+            this.labelComboText3.BackColor = System.Drawing.Color.Transparent;
+            this.labelComboText3.BoxKind = BedFactory.LabelComboText.Kind.combo;
+            this.labelComboText3.ComboData = null;
+            this.labelComboText3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.labelComboText3.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.labelComboText3.Header = null;
+            this.labelComboText3.Important = false;
+            this.labelComboText3.Location = new System.Drawing.Point(867, 3);
+            this.labelComboText3.MultiLine = false;
+            this.labelComboText3.MultiLineCnt = 0;
+            this.labelComboText3.Name = "labelComboText3";
+            this.labelComboText3.Size = new System.Drawing.Size(398, 31);
+            this.labelComboText3.TabIndex = 9;
             // 
-            // label7
+            // button3
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label7.Location = new System.Drawing.Point(1051, 7);
-            this.label7.Margin = new System.Windows.Forms.Padding(7);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 23);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "label7";
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(1163, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 25);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = false;
             // 
-            // label5
+            // button6
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(539, 7);
-            this.label5.Margin = new System.Windows.Forms.Padding(7);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 23);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "label5";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(522, 10);
-            this.label4.Margin = new System.Windows.Forms.Padding(10, 10, 10, 3);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(17, 23);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "*";
+            this.button6.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button6.Location = new System.Drawing.Point(1234, 99);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 25);
+            this.button6.TabIndex = 8;
+            this.button6.UseVisualStyleBackColor = false;
             // 
             // BaseForm4
             // 
@@ -204,6 +205,8 @@ namespace BedFactory.BaseForms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(1382, 853);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
@@ -213,25 +216,23 @@ namespace BedFactory.BaseForms
             this.Name = "BaseForm4";
             this.Text = "BaseForm4";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         protected System.Windows.Forms.Panel panel1;
         protected System.Windows.Forms.Panel panel2;
         protected System.Windows.Forms.Button button1;
         protected System.Windows.Forms.Button button4;
         protected System.Windows.Forms.Button button5;
         protected System.Windows.Forms.Label label1;
-        protected System.Windows.Forms.Label label7;
-        protected System.Windows.Forms.Label label5;
-        protected System.Windows.Forms.Label label2;
         protected System.Windows.Forms.Button button2;
+        protected LabelComboText labelComboText3;
+        protected LabelComboText labelComboText2;
+        protected System.Windows.Forms.Button button3;
+        protected System.Windows.Forms.Button button6;
+        protected LabelComboText labelComboText1;
     }
 }
