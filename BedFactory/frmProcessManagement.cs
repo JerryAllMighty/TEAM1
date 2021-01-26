571bf17fa039536bf9f66e5d4dac3ed79c116d2e
경진 장 <jkj04032000@gmail.com>
2021-01-26 20:56:43 +0900

    공정관리

---
+++ BedFactory/frmProcessManagement.cs
@@ -0,0 +1,65 @@
+﻿using BedFactory.Util;
+using BedFactoryService;
+using BedFactoryVO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BedFactory
+{
+    public partial class frmProcessManagement : BedFactory.BaseForms.BaseForm1
+    {
+        public frmProcessManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFacilitiesManagement_Load(object sender, EventArgs e)
+        {
+            //컬럼추가
+            //그리드뷰 바인딩
+            CommonUtil.SetInitGridView(datagridviewControl1);
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "공정상세번호", "Process_D_Name");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "공정상세명", "Process_D_Name");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "공정조건", "Process_D_Condition");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "최초등록자", "Firstman");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "최초등록일", "Firstdate");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "최종등록자", "Lastman");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "최종수정일", "Lastdate");
+            CommonUtil.AddGridTextColumn(datagridviewControl1, "정보삭제여부", "IsDeleted");
+
+
+            //콤보박스 바인딩
+        }
+
+
+        //조회
+        //public override void btnSelect_Click(object sender, EventArgs e)
+        //{
+        //    string procCate = ""; //cboName.SelectedValue.ToString();
+        //    string procdNum = "";
+
+        //    ProcessService service = new ProcessService();
+        //    List<ProcessDetailVO> list = service.GetProcessDetailInfo(procCate, procdNum);
+        //    datagridviewControl1.DataSource = list;
+        //}
+
+
+        // 등록버튼
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // 삭제버튼
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BedFactoryDAC/ProcessDAC.cs
@@ -0,0 +1,74 @@
+﻿using BedFactoryVO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedFactoryDAC
+{
+    public class ProcessDAC : ConnectionAccess, IDisposable
+    {
+        string strConn;
+        SqlConnection Conn;
+
+        public ProcessDAC()
+        {
+            strConn = this.ConnectionString;
+            Conn = new SqlConnection(strConn);
+            Conn.Open();
+        }
+
+        public void Dispose()
+        {
+            if (Conn != null)
+            {
+                this.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        /// 검색조건에 따른 설비군 정보 조회
+        /// </summary>
+        /// <param name="fcsName">공정분류</param>
+        /// <param name="fcsNum">공정상세번호</param>
+        /// <returns></returns>
+        /// 
+        public List<ProcessDetailVO> GetProcessDetailInfo(string procCate, string procdNum)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"select P.Process_Category, Process_D_Num, D.Process_Num, Process_D_Name, Process_D_Condition, 
+                       D.Firstman, D.Lastman, CONVERT(varchar(10) , D.Firstdate, 23) Firstdate, 
+	                   CONVERT(varchar(10) , D.Lastdate, 23) Lastdate, D.IsDeleted
+                       from tblProcess P inner join tblProcess_D D on D.Process_Num = P.Process_Num
+                       where 1 = 1");
+
+            if (!string.IsNullOrEmpty(procCate))
+                sb.Append(" and P.Process_Category = @procCate");
+            if (!string.IsNullOrEmpty(procdNum))
+                sb.Append(" and Process_D_Num = @procdNum");
+
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.CommandText = sb.ToString();
+                cmd.Connection = Conn;
+
+                cmd.Parameters.AddWithValue("@P.Process_Category", procCate);
+                cmd.Parameters.AddWithValue("@Process_D_Num", procdNum);
+
+                List<ProcessDetailVO> list = Helper.DataReaderMapToList<ProcessDetailVO>(cmd.ExecuteReader());
+                Conn.Close();
+
+                return list;
+            }
+        }
+
+        //설비군 삭제
+        public string DeleteFacilities()
+        {
+            return null;
+        }
+    }
+}
