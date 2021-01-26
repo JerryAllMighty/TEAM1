7227deb6e153384093fa412e91a1e800d1df53a1
경진 장 <jkj04032000@gmail.com>
2021-01-27 04:06:39 +0900

    커밋

---
+++ BedFactoryDAC/AuthorityDAC.cs
@@ -0,0 +1,49 @@
+﻿using BedFactoryVO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedFactoryDAC
+{
+    public class AuthorityDAC : ConnectionAccess, IDisposable
+    {
+        string strConn;
+        SqlConnection conn;
+
+        public AuthorityDAC()
+        {
+            strConn = this.ConnectionString;
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public List<AuthorityVO> GetAuthorityInfo()
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"select Auth_Num, ListNum, Auth_Name, FirstMan, FirstDate, LastMan,LastDate, IsDeleted
+                                        from tblAuthority";
+                     List<AuthorityVO> list = Helper.DataReaderMapToList<AuthorityVO>(cmd.ExecuteReader());
+
+                    return list != null ? list : null;
+                }
+            }
+            catch (Exception err)
+            {
+                Log.WriteError(err.Message);
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ BedFactoryVO/AuthorityVO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedFactoryVO
+{
+    public class AuthorityVO
+    {
+        public int ListNum { get; set; }
+        public string Auth_Department { get; set; }
+        //public int LFirstMan { get; set; }
+        //public string LFirstDate { get; set; }
+        //public int LLastMan { get; set; }
+        //public string LLastDate { get; set; }
+        //public string LIsDeleted { get; set; }
+        public int Auth_Num { get; set; }
+        public string Auth_Name { get; set; }
+        public int FirstMan { get; set; }
+        public DateTime FirstDate { get; set; }
+        public int LastMan { get; set; }
+        public DateTime LastDate { get; set; }
+        public string IsDeleted { get; set; }
+
+    }
+}
