267ecf4981b5c0757e0c3fc7904b46cecc4f7b86
경진 장 <jkj04032000@gmail.com>
2021-01-26 18:00:33 +0900

    베이스폼2 (레이아웃테이블 상속x)

--- BedFactory/frmUserGroupAuthority.cs
+++ BedFactory/frmUserGroupAuthority.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace BedFactory
 {
-    public partial class frmUserGroupAuthority : BedFactory.BaseForms.BaseForm4
+    public partial class frmUserGroupAuthority : BedFactory.BaseForms.BaseForm1
     {
         public frmUserGroupAuthority()
         {
